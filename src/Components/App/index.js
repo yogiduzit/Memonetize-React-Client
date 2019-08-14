d5d8ef84cf5e2110eb6eaebd2679fb87d7f364d1
yogiduzit <yverma2@my.bcit.ca>
2019-08-14 15:14:43 -0700

    Created redux store

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import Router from '../Router'
+import { createStore, applyMiddleware } from 'redux';
+import handleSession from '../../store/reducers/reducers'
+import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import { signInUser } from '../../store/actions';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -10,9 +16,13 @@ export default class App extends React.Component {
     }
   }
 
+
   render() {
+    const store = createStore(handleSession, composeWithDevTools(applyMiddleware(thunk)));
     return(
+
       <article className="Yogi">
+        <Provider store={store}></Provider>
         <Router>
         </Router>
       </article> 
