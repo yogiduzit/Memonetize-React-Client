97c32cdc45e8507184452e699d27c794241d1021
yogiduzit <yverma2@my.bcit.ca>
2019-08-22 23:58:10 -0700

    Fixed redirection on all pages

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 4000
 
 const app = express();
 
--- src/Components/AuthRoute/index.js
+++ src/Components/AuthRoute/index.js
@@ -3,9 +3,6 @@ import {Route, Redirect} from 'react-router-dom';
 
 export default function AuthRoute(props) {
   const {isAuth, component: Component, ...routeProps} = props;
-  if (props.isAuth) {
-    return(<Route {...routeProps} component={Component}/>);
-  } else {
-    return (<Redirect to="/sessions/new"/>);
-  }
+
+  return(<Route {...routeProps} render={(props) => (isAuth) ? <Component {...props} />: <Redirect to="/sessions/new"/>}/>)
 }
